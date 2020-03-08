fb5e37ab30477e8224c77a9dd8c9db3a524f3555
chuanguoyuxi <41041388+chuanguoyuxi@users.noreply.github.com>
2020-03-08 18:23:10 +0800

    my-kbone

---
+++ src/common/footers.vue
@@ -0,0 +1,46 @@
+<template>
+  <KView class="">
+    <KView v-if="isFill" class="isFill"></KView>
+    <KFlex class="footers">
+      <KFlexItem>
+        <KView class="placeholder">首页</KView>
+      </KFlexItem>
+      <KFlexItem>
+        <KView class="placeholder">文档</KView>
+      </KFlexItem>
+      <KFlexItem>
+        <KView class="placeholder">我的</KView>
+      </KFlexItem>
+    </KFlex>
+  </KView>
+</template>
+
+<script>
+import Vue from 'vue'
+// eslint-disable-next-line no-unused-vars
+import { KView, KFlex, KFlexItem } from 'kbone-ui'
+
+export default Vue.extend({
+  name: 'footers',
+  props: {
+    isFill: { type: Boolean, default: true } // 是否显示填充div
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+})
+</script>
+<style scoped>
+.footers {
+  text-align: center;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  background: #ffffff;
+}
+.isFill {
+  width: 100%;
+  height: 65px;
+}
+</style>
---
+++ src/common/list/list.vue
@@ -0,0 +1,37 @@
+<template>
+  <KView class="list">
+    <!-- <KCells>
+      <KCell v-for="v in list" :key="v.id">
+        <KIcon slot="icon" type="info" />
+      </KCell>
+    </KCells> -->
+    <KView  v-for="v in list" :key="v.id" class="weui-media-box weui-media-box_appmsg">
+        <div class="weui-media-box__hd">
+            <img class="weui-media-box__thumb" :src="v.imgsrc" alt="">
+        </div>
+        <div class="weui-media-box__bd">
+            <h4 class="weui-media-box__title">{{v.title}}</h4>
+            <p class="weui-media-box__desc">{{v.content}}</p>
+        </div>
+    </KView>
+  </KView>
+</template>
+
+<script>
+import Vue from 'vue'
+// eslint-disable-next-line no-unused-vars
+import { KView, KCells, KCell, KIcon } from 'kbone-ui'
+
+export default Vue.extend({
+  name: 'list',
+  components: {
+  },
+  props: {
+    list: { type: Array }
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+})
+</script>
